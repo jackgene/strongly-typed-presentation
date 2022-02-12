23a0b84f006c1734429356acbe8d2f30b31bf92d
Jack Leow <jleow@goodrx.com>
2022-02-12 06:33:38 -0800

    Tweaked animations and some text. Establish WebSocket connection only when necessary.

--- app/assets/javascripts/Deck/Main.elm
+++ app/assets/javascripts/Deck/Main.elm
@@ -8,13 +8,13 @@ import Css exposing
   , width, overflow, paddingTop, position, right, top
   -- Content
   , backgroundColor, before, color, fontFamilies, fontSize, fontWeight
-  , lineHeight, textAlign, visibility
+  , lineHeight, opacity, textAlign, visibility
   -- Sizes
-  , auto, em, int, pct, vw, zero
+  , auto, em, int, num, pct, vw, zero
   -- Positions
   , absolute, relative, static
   -- Other values
-  , block, center, hidden, rgb, visible
+  , block, center, hidden, none, rgb, visible
   )
 import Css.Transitions exposing (easeInOut, transition)
 import Deck.Common exposing (..)
@@ -34,6 +34,7 @@ type alias SlideModel =
   { init : Model -> (Model, Cmd Msg)
   , update : Msg -> Model -> (Model, Cmd Msg)
   , view : Model -> Html Msg
+  , liveUpdate : Bool
   }
 
 
@@ -90,6 +91,7 @@ slideTemplate =
   { init = ( \model -> (model, Cmd.none) )
   , update = ( \_ model -> (model, Cmd.none) )
   , view = ( \_ -> text "(Placeholder)" )
+  , liveUpdate = False
   }
 
 
@@ -105,12 +107,13 @@ slides =
         , h1
           [ css
             [ position absolute
-            , top (pct 20), left (pct 36)
+            , top (vw 18), left (vw 36)
+            , width (vw 55)
             , fontFamilies [ "GoodRx Moon", "sans-serif" ]
-            , fontSize (vw 7)
+            , fontSize (vw 4.6)
             ]
           ]
-          [ text "Zoom Chat Behavior" ]
+          [ text "What Does It Mean for a Programming Language to Be Strongly Typed?" ]
         ]
       )
     }
@@ -142,88 +145,97 @@ slides =
               , transition [ Css.Transitions.padding3 500 0 easeInOut ]
               ]
             ]
-            [ text "What is your prefered programming language?" ]
-          , div []
-            ( if List.isEmpty model.languagesAndCounts then
-                [ p
-                  [ css
-                    [ fontFamilies [ "GoodRx Bolton", "sans-serif" ]
-                    , fontSize (vw 2.2) ]
-                    ]
-                  [ text "In other words, the one that you..."
-                  , ul []
-                    [ li [] [ text "...are most familiar with" ]
-                    , li [] [ text "...would select for personal projects" ]
-                    , li [] [ text "...would choose to be tested on in a technical interview" ]
-                    ]
-                  ]
+            [ text "What is your preferred programming language?" ]
+          , div
+            [ css
+              [ height (vw (if List.isEmpty model.languagesAndCounts then 20 else 0))
+              , overflow hidden
+              , opacity (num (if List.isEmpty model.languagesAndCounts then 1.0 else 0))
+              , transition
+                [ Css.Transitions.height3 500 0 easeInOut
+                , Css.Transitions.opacity3 500 0 easeInOut
                 ]
-              else
-                [ p
-                  [ css
-                    [ fontFamilies [ "GoodRx Bolton", "sans-serif" ]
-                    , fontSize (vw 2.2) ]
-                    ]
-                  [ text ("The GoodRx Software Engineering Top " ++ (toString maxDisplayCount) ++ ":") ]
-                , ( Keyed.node "div" [ css [ position relative ] ]
-                    ( let
-                        maxCount : Int
-                        maxCount =
-                          Maybe.withDefault 0
-                          ( Maybe.map Tuple.second (List.head model.languagesAndCounts) )
-                      in
-                      List.sortBy Tuple.first
-                      ( List.indexedMap
-                        ( \idx (language, count) ->
-                          ( language
-                          , div
-                            [ id (idEscape language)
-                            , css
-                              [ if idx < maxDisplayCount then visibility visible else visibility hidden
-                              , position absolute
-                              , top (em (toFloat idx * 2))
-                              , width (pct 80)
-                              , fontFamilies [ "GoodRx Bolton", "sans-serif" ], fontSize (vw 1.6)
-                              , lineHeight (em 1.6)
-                              , transition
-                                [ Css.Transitions.top3 500 0 easeInOut
-                                , Css.Transitions.visibility3 500 0 easeInOut
-                                ]
-                              ]
+              ]
+            ]
+            [ p
+              [ css
+                [ margin2 zero zero
+                , fontFamilies [ "GoodRx Bolton", "sans-serif" ]
+                , fontSize (vw 2.2) ]
+                ]
+              [ text "In other words, the language you:"
+              , ul []
+                [ li [] [ text "Are most familiar with..." ]
+                , li [] [ text "Would use for personal projects..." ]
+                , li [] [ text "Would want to be quizzed on in a technical interview..." ]
+                , li [] [ text "May or may not use daily at GoodRx..." ]
+                ]
+              ]
+            ]
+          , div [ css [ if List.isEmpty model.languagesAndCounts then display none else display block ] ]
+            [ p
+              [ css
+                [ margin2 zero zero
+                , fontFamilies [ "GoodRx Bolton", "sans-serif" ]
+                , fontSize (vw 2.2) ]
+                ]
+              [ text
+                ("The Top "
+                ++(toString (min maxDisplayCount (List.length model.languagesAndCounts)))
+                ++" Programming Languages at GoodRx:"
+                )
+              ]
+            , ( Keyed.node "div" [ css [ position relative ] ]
+                ( let
+                    maxCount : Int
+                    maxCount =
+                      Maybe.withDefault 0
+                      ( Maybe.map Tuple.second (List.head model.languagesAndCounts) )
+                  in
+                  List.sortBy Tuple.first
+                  ( List.indexedMap
+                    ( \idx (language, count) ->
+                      ( language
+                      , div
+                        [ id (idEscape language)
+                        , css
+                          [ if idx < maxDisplayCount then visibility visible else visibility hidden
+                          , position absolute
+                          , top (em (toFloat (1 + (idx * 2))))
+                          , width (pct 90)
+                          , fontFamilies [ "GoodRx Bolton", "sans-serif" ], fontSize (vw 1.6)
+                          , lineHeight (em 1.6)
+                          , transition
+                            [ Css.Transitions.top3 500 0 easeInOut
+                            , Css.Transitions.visibility3 500 0 easeInOut
                             ]
-                            [ div
-                              [ css
-                                [ position absolute
-                                , top zero
-                                , width (pct 22)
-                                , textAlign right
-                                ]
-                              ]
-                              [ Maybe.withDefault
-                                ( text language )
-                                ( Dict.get language logosByLanguage )
-                              ]
-                            , div
-                              [ css
-                                [ position absolute
-                                , top zero
-                                , left (pct 23)
-                                , right zero
-                                ]
-                              ]
-                              [ horizontalBarView count maxCount ]
+                          ]
+                        ]
+                        [ div
+                          [ css
+                            [ position absolute, top zero, width (pct 16)
+                            , textAlign right
                             ]
-                          )
-                        )
-                        model.languagesAndCounts
+                          ]
+                          [ Maybe.withDefault
+                            ( text language )
+                            ( Dict.get language logosByLanguage )
+                          ]
+                        , div
+                          [ css [ position absolute, top zero, left (pct 17), right zero ] ]
+                          [ horizontalBarView count maxCount ]
+                        ]
                       )
                     )
+                    model.languagesAndCounts
                   )
-                ]
-            )
+                )
+              )
+            ]
           ]
         ]
       )
+    , liveUpdate = True
     }
   , Slide
     { slideTemplate
@@ -436,9 +448,19 @@ view model =
 subscriptions : Model -> Sub Msg
 subscriptions model =
   Sub.batch
-  [ case model.eventsWsUrl of
-    Just url -> WebSocket.listen url Event
-    Nothing -> Sub.none
+  [ let
+      connectWebSocket : Bool
+      connectWebSocket =
+        Maybe.withDefault False
+        ( Maybe.map
+          ( \(Slide slide) -> slide.liveUpdate )
+          ( Array.get model.slideIndex model.slides )
+        )
+    in
+    case (model.eventsWsUrl, connectWebSocket) of
+      (Just url, True) ->
+        WebSocket.listen url Event
+      _ -> Sub.none
   , Keyboard.ups
     ( \keyCode ->
       case keyCode of
