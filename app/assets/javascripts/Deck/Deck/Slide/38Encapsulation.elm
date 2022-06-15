53b63aa0397932aed8f0c005de3f5ca769cb1c04
Jack Leow <jleow@goodrx.com>
2022-06-15 06:46:10 -0700

    Minor Go code tweak.

--- app/assets/javascripts/Deck/Deck/Slide/38Encapsulation.elm
+++ app/assets/javascripts/Deck/Deck/Slide/38Encapsulation.elm
@@ -112,7 +112,7 @@ package main
 import "strongly-typed-go/encapsulation/counter"
 
 func main() {
-    c := counter.Counter{}
+    c := &counter.Counter{}
     c.count = -1
 
     print("Count: ", c.Count())
